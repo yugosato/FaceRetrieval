3c50e0bf00461de50008ae37217ceeafab089b66
yugosato <yg.physics72@gmail.com>
2017-04-07 17:29:24 +0900

    modified gitignore

--- obj/linux64/Release/src/NGT/Graph.d
+++
@@ -1,2 +0,0 @@
-obj/linux64/Release/src/NGT/Graph.o: \
- /home/yugo/workspace/Interface/src/NGT/Graph.cpp
--- obj/linux64/Release/src/NGT/SharedMemoryAllocator.d
+++
@@ -1,2 +0,0 @@
-obj/linux64/Release/src/NGT/SharedMemoryAllocator.o: \
- /home/yugo/workspace/Interface/src/NGT/SharedMemoryAllocator.cpp
