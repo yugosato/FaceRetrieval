bd220def7265e6d2c05485438709fe206f8e7825
yugosato <yg.physics72@gmail.com>
2017-04-17 17:53:08 +0900

    3次元可視化

--- visualize/visualize_for_evaluation.py
+++ visualize/visualize_for_evaluation.py
@@ -2,18 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import os
+import numpy as np
 from sklearn.externals import joblib
 import matplotlib.pyplot as plt
-
-
-inputdir = '../bin/data/lfw'
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def main():
-    inputfile = os.path.join(inputdir, 'lfw-vgg_center_cos-mds.pkl')
+    inputdir = '../bin/data/lfw'
+    inputfile = os.path.join(inputdir, 'lfw-vgg_center-tsne.npy')
+    X = np.load(inputfile)
 
-    pos = joblib.load(inputfile)
-    plt.scatter(pos[:,0], pos[:,1])
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.set_xlabel("X-axis")
+    ax.set_ylabel("Y-axis")
+    ax.set_zlabel("Z-axis")
+    ax.plot(X[:, 0], X[:, 1], X[:, 2], "o", color="b", ms=4, mew=0.5)
     plt.show()
     
 if __name__ == '__main__':
