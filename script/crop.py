69f7a3eed723b115c93900f31acc9bf530e285c9
yugosato <yg.physics72@gmail.com>
2017-04-24 18:08:01 +0900

    cropping cfd

--- script/crop.py
+++ script/crop.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 
-'''
-Cropping and Resize lfw dataset
-'''
-
 import os
 import dlib
 import cv2
+import glob
 
 
 detector = dlib.get_frontal_face_detector()
@@ -30,34 +27,45 @@ def resize(img):
     return cv2.resize(img, (224, 224))
     
 
-def crop_face(directory, out='cropped'):
-    person = directory[directory.rfind('\\')+1:]
-    images = os.listdir(directory)
-   
-    for i, image in enumerate(images):
-        print image
-        image_path = os.path.join(directory, image)
-        img = cv2.imread(image_path)
-        height, width = img.shape[:2]
-        rect = facedetect_dlib(img)
-        
-        if len(rect) > 0:            
-            try:
-                os.makedirs(os.path.join(out, person))
-            except:
-                pass 
-            
-            for j, nrect in enumerate(rect):
-                if not (nrect.left() < 0 or nrect.top() < 0 or nrect.right() > width or nrect.bottom() > height):
-                    dst = resize(crop(img, nrect))
-                    cv2.imwrite(os.path.join(out, person, person+'_{0:04d}-{1}.jpg'.format(i+1,j+1)), dst)
-        else:
-            continue
-        
+def crop_face(directory, outdir):
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    persons = os.listdir(directory)
+    persons.sort()
+
+    failed = open(os.path.join(directory, 'failed.txt'), 'a')
+
+    for person in persons:
+        if not os.path.exists(os.path.join(outdir, person)):
+            os.makedirs(os.path.join(outdir, person))
+
+        target = os.path.join(directory, person, '*.*')
+        images = glob.glob(target)
+        images.sort()
+
+        for image in images:
+            print image
+            filename = image[image.rfind('/')+1:]
+            input_img = cv2.imread(image)
+            height, width = input_img.shape[:2]
+            rect = facedetect_dlib(input_img)
+
+            if len(rect) > 0:
+                for j, nrect in enumerate(rect):
+                    if not (nrect.left() < 0 or nrect.top() < 0 or nrect.right() > width or nrect.bottom() > height):
+                        dst_img = resize(crop(input_img, nrect))
+                        cv2.imwrite(os.path.join(outdir, person, filename), dst_img)
+            else:
+                failed.writelines(image)
+                continue
+
     
-def walk_crop_face(images_dir):
-    map(lambda f: crop_face(os.path.join(images_dir, f)), next(os.walk(images_dir))[1])
+def walk_crop_face(images_dir, outdir):
+    map(lambda f: crop_face(os.path.join(images_dir, f), outdir), next(os.walk(images_dir))[1])
    
     
 if __name__ == '__main__':
-    walk_crop_face('lfw')
+    inputdir = '/home/yugo/Desktop/cfd/CFD Version 2.0.3'
+    outdir = '/home/yugo/Desktop/cfd/CFD Version 2.0.3/cropped'
+    walk_crop_face(inputdir, outdir)
