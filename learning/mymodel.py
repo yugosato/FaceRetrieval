22f971ec30eba3ab8e72c3852eb989dfa2bb0e28
yugosato <yg.physics72@gmail.com>
2017-05-28 16:11:18 +0900

    change parameter

--- learning/mymodel.py
+++ learning/mymodel.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import chainer
-from chainer import Chain
 import chainer.functions as F
 import chainer.links as L
+from chainer import Chain
 
 
 class MyModel(Chain):
-
     def __init__(self):
         super(MyModel, self).__init__(
             fc1=L.Linear(None, 4096),
-            fc2=L.Linear(None, 1024),
+            fc2=L.Linear(None, 4096),
             fc3=L.Linear(None, 2)
         )
         self.train = True
 
         
     def __call__(self, x, t):
-        h = F.relu(self.fc1(x))
-        h = F.relu(self.fc2(h))
+        h = F.dropout(F.relu(self.fc1(x)), train=self.train, ratio=0.5)
+        h = F.dropout(F.relu(self.fc2(h)), train=self.train, ratio=0.5)
         h = self.fc3(h)
-        
+
         loss = F.softmax_cross_entropy(h, t)
         chainer.report({'loss': loss, 'accuracy': F.accuracy(h, t)}, self)
         return loss
--- learning/trainmodel.py
+++ learning/trainmodel.py
@@ -7,37 +7,50 @@ from dataset import ImportDataset
 import mymodel
 
 
-def train():
+def train_model():
     # File paths
-    listfile = "../bin/log/feedback.txt"
-    inputfile = "../bin/data/cfd/cfd-vgg.npy"
+    listfile = "/home/yugo/workspace/Interface/bin/log/feedback.txt"
+    inputfile = "/home/yugo/workspace/Interface/bin/data/cfd/cfd-vgg.npy"
 
     # Training parameter
-    epoch = 10
-    minibatch_size = 5
+    epoch = 1
+    batch_size = 5
+    gpu_id = 0
 
     # Initialize model to train
     model = mymodel.MyModel()
+    chainer.datasets.LabeledImageDataset
+    if gpu_id >= 0:
+        chainer.cuda.get_device_from_id(gpu_id).use()
+        model.to_gpu()
 
     # Load datasets and set up iterator
     train = ImportDataset(listfile, inputfile)
-    train_iter = chainer.iterators.SerialIterator(train, batch_size=minibatch_size)
+    train_iter = chainer.iterators.SerialIterator(train, batch_size=batch_size)
 
     # Set optimizer
-    optimizer = chainer.optimizers.Adam()
+    optimizer = chainer.optimizers.AdaDelta()
     optimizer.setup(model)
 
+    # # Copy chain with shared parameters to flip 'train' flag only in test
+    # eval_model = model.copy()
+    # eval_model.train = False
+
     # Set up updateer and trainer
-    updater = training.StandardUpdater(train_iter, optimizer)
+    updater = training.StandardUpdater(train_iter, optimizer, device=gpu_id)
     trainer = training.Trainer(updater, (epoch, 'epoch'), 'result')
     trainer.extend(extensions.LogReport())
     trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'main/accuracy']))
-    trainer.extend(extensions.ProgressBar())
+    trainer.extend(extensions.PlotReport(['main/loss'], 'epoch', file_name='loss.png'))
+    trainer.extend(extensions.PlotReport(['main/accuracy'], 'epoch', file_name='accuracy.png'))
 
     # Run trainer
     trainer.run()
-    
+
+    print model.fc2.W.data.shape
+    print model.fc2.b.data.shape
+
 
 if __name__ == '__main__':
-    train()
+    train_model()
     
