5b97683f7c90904da65d3c9cf693c1f5f92d6b2d
yugosato <yg.physics72@gmail.com>
2017-07-22 18:25:36 +0900

    minor

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -456,7 +456,7 @@ void ofApp::keyPressed(int key)
 		{
 			ofImage img;
 			img.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
-			std::string savepath = "/home/yugo/workspace/Interface/bin/snapshot/" + ofGetTimestampString() + ".png";
+			std::string savepath = "/home/yugo/workspace/Interface/bin/snapshot/" + ofGetTimestampString() + ".jpeg";
 			img.save(savepath);
 			std::cout << "[ofApp] saved snapshot image." << std::endl;
 			std::cout << "[ofApp] --> \"" << savepath << "\"" << std::endl;
