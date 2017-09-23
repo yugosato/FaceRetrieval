55873ad181ef4c138c5b5dba85b980e2f31ff22e
yugosato <yg.physics72@gmail.com>
2017-09-23 21:03:39 +0900

    draw acc-val graph

--- trainer/trainmodel.py
+++ trainer/trainmodel.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import matplotlib.pyplot as plt
 import cupy as xp
+import numpy as np
 import chainer
 from chainer import training
 from chainer import serializers
@@ -23,14 +25,35 @@ weight = None
 bias = None
 model = None
 
+def drawGraph(acc, val):
+    total_iter_num = 10
+    len_acc = len(acc)
+    x = [i for i in xrange(total_iter_num)]
+
+    plt.figure(figsize=(11,3))
+    plt.fill_between(x[:len_acc], acc - val, acc + val, color="blue", alpha=0.1, label="variance")
+    plt.plot(x[:len_acc], acc, color="b", alpha=0.5, label="reliability")
+    plt.ylim([0.0, 1.0])
+    plt.xlim([0, 10])
+    plt.tight_layout(pad=2)
+    plt.legend()
+    plt.xlabel("Iteration")
+    plt.ylabel("Reliability")
+    plt.yticks(np.arange(0.0, 1.1, 0.1))
+    plt.xticks(np.arange(0, 11, 1))
+    plt.savefig(os.path.join(home_dir, "result", "acc_val.png"))
+
+
 def variance_score(acc, size):
     return acc * (1 - acc) / size
+
 
 def remove(dirpath):
     if os.path.exists(dirpath):
         files = os.listdir(dirpath)
         for file in files:
             os.remove(os.path.join(dirpath, file))
+
 
 def train_model():
     # Remove old files
@@ -103,7 +126,7 @@ def train_model():
     predicted_labels = []
 
     for train_index, [test_index] in loo.split(split_samples.input_base_):
-        print "[LOOCV] Test {}, Train: {}.".format(test_index, train_index)
+        print "[LOOCV] Test: {}, Train: {}.".format(test_index, train_index)
 
         # Initialize model to train
         model_val = mymodel.MyModel(unit)
@@ -137,9 +160,23 @@ def train_model():
         true_labels.append(true_label)
         predicted_labels.append(predicted_label)
 
+    # Calculate accuracy and valiance
     acc = accuracy_score(true_labels, predicted_labels)
     val = variance_score(acc, len(split_samples.input_base_))
+
+    if (train.iter_num_ - 1) == 1:
+        if os.path.exists(os.path.join(home_dir, "acc_val.npy")):
+            os.remove(os.path.join(home_dir, "acc_val.npy"))
+        acc_val = np.array([[0.0], [0.0]])
+    else:
+        acc_val = np.load(os.path.join(home_dir, "acc_val.npy"))
+
+    # Save as figure.
+    acc_val = np.append(acc_val, [[acc], [val]], axis=1)
+    np.save(os.path.join(home_dir, "acc_val.npy"), acc_val)
+    drawGraph(acc_val[0], acc_val[1])
     print "[LOOCV] Accuracy(average): {}, Variance: {}.".format(acc, val)
+
 
 
 def feature_extract(x):
